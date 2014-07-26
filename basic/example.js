362cfe2c7e4c5ce15750fc545e3fc6438a37221a
nie xin <niexin.study@gmail.com>
2014-07-26 16:18:18 +0200

    refactoring

--- basic/example.js
+++ basic/example.js
@@ -1,10 +1,27 @@
-var canvas = document.getElementById('canvas'),
-    context = canvas.getContext('2d');
+var HELLOCANVAS = (function() {
 
-context.font = '38pt Arial';
-context.fillStyle = 'cornflowerblue';
-context.strokeStyle = 'blue';
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d');
 
-context.fillText('Hello Canvas', canvas.width/2 - 150, canvas.height/2 + 15);
-context.strokeText('Hello Canvas', canvas.width/2 - 150, canvas.height/2 + 15);
+    var init = function() {
+        context.font = '38pt Arial';
+        context.fillStyle = 'cornflowerblue';
+        context.strokeStyle = 'blue';
+    };
+
+    var draw = function() {
+        context.fillText('Hello Canvas', canvas.width/2 - 150, canvas.height/2 + 15);
+        context.strokeText('Hello Canvas', canvas.width/2 - 150, canvas.height/2 + 15);
+    };
+
+    return {
+        init: init,
+        draw: draw
+    };
+
+})();
+
+// run hello example
+HELLOCANVAS.init();
+HELLOCANVAS.draw();
 
